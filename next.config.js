63cc624f25ffdd5e4eedc1bcd4b98963b205200c
felipe1496 <felipe.oliveira1496@gmail.com>
2022-07-28 22:16:44 -0300

    feat: adding i18n config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,11 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+
+  i18n: {
+    locales: ['en-US', 'pt-BR'],
+    defaultLocale: 'pt-BR',
+  },
 };
 
 module.exports = nextConfig;
