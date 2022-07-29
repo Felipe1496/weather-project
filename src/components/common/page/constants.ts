922955fd822d8460ec375b163c0948b3e2453ef7
felipe1496 <felipe.oliveira1496@gmail.com>
2022-07-28 22:14:39 -0300

    feat: adding application name

--- src/components/common/page/constants.ts
+++ src/components/common/page/constants.ts
@@ -1,3 +1,3 @@
-export const APPLICATION_NAME = '';
+export const APPLICATION_NAME = 'Weather Project';
 
-export const DEFAULT_PAGE_DESCRIPTION = '';
+export const DEFAULT_PAGE_DESCRIPTION = 'Oferecemos uma plataforma de consulta do clima de inúmeras localizações';
