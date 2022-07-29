5e24fe7cc001936f403d2cf429ff8713c455381a
felipe1496 <felipe.oliveira1496@gmail.com>
2022-07-28 22:07:07 -0300

    feat: adding tailwindcss configs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,38 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+const tailwindConfig = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          light: '#ffffff',
+        },
+        secondary: {
+          dark: '#6AA2D1',
+        },
+        tertiary: {
+          light: '#D2B3C1',
+        },
+      },
+
+      fontSize: {
+        xs: '0.625rem',
+        sm: '0.75rem',
+        md: '0.875',
+        lg: '1rem',
+        xl: '1.25rem',
+        '2xl': '1.8125rem',
+        '3xl': '2.6875rem',
+        '4xl': '4rem',
+      },
+
+      fontFamily: {
+        sans: ['Montserrat', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 };
+
+module.exports = tailwindConfig;
