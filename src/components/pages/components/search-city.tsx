ce3d40a1471e3746999db36a3310f5a65e7e8c51
felipe1496 <felipe.oliveira1496@gmail.com>
2022-07-31 08:20:13 -0300

    feat: search by city and global white text

--- src/components/pages/components/search-city.tsx
+++ src/components/pages/components/search-city.tsx
@@ -1,13 +1,21 @@
 import { FC } from 'react';
 
+import Autocomplete from '@/components/common/AutoComplete/auto-complete';
 import { useLanguage } from '@/hooks/useLanguage';
 
-const SearchCity: FC = () => {
+interface Props {
+  // eslint-disable-next-line no-unused-vars
+  setStep: (step: number) => void;
+}
+
+const SearchCity: FC<Props> = ({ setStep }) => {
   const { translatable } = useLanguage();
 
   return (
-    <div>
-      <h1 className="text-center text-2xl font-bold text-primary-light">{translatable().titles.time}</h1>
+    <div className="flex flex-col">
+      <h1 className="text-center text-2xl font-bold md:tracking-widest">{translatable().titles.time}</h1>
+      <div className="h-3" />
+      <Autocomplete setStep={setStep} />
     </div>
   );
 };
